1a93677740f70acd37d506dae34df529ef3606e9
rawsashimi1604 <looweiren@gmail.com>
2023-09-17 21:58:16 +0800

    able to create meeting

--- ZoomService.js
+++ ZoomService.js
@@ -18,9 +18,35 @@ async function getAccessToken() {
     },
   };
   const res = await axios.post("https://zoom.us/oauth/token", data, config);
-  console.log(res.data);
+  return res.data;
+}
+
+async function createMeeting(userId, token) {
+  const config = {
+    headers: {
+      Host: "zoom.us",
+      Authorization: "Bearer " + token,
+    },
+  };
+
+  const startTime = new Date();
+  startTime.setHours(22, 0, 0, 0);
+
+  const body = {
+    agenda: "Some random Agenda",
+    join_before_host: true,
+    start_time: startTime.toISOString(),
+  };
+
+  const res = await axios.post(
+    `https://api.zoom.us/v2/users/${userId}/meetings`,
+    body,
+    config
+  );
+  return res.data;
 }
 
 export default {
   getAccessToken,
+  createMeeting,
 };
--- index.js
+++ index.js
@@ -8,8 +8,18 @@ app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
 
-app.post("/zoom/token", (req, res) => {
-  ZoomService.getAccessToken();
+app.post("/zoom/token", async (req, res) => {
+  const token = await ZoomService.getAccessToken();
+  res.json({ token });
+});
+
+app.post("/zoom/meeting", async (req, res) => {
+  const token = await ZoomService.getAccessToken();
+  const meeting = await ZoomService.createMeeting(
+    "looweiren@gmail.com",
+    token.access_token
+  );
+  res.json({ meeting });
 });
 
 app.listen(port, () => {
