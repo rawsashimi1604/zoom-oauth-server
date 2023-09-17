9e60b898f070a500498b18118ff7ef0f648a7053
rawsashimi1604 <looweiren@gmail.com>
2023-09-17 21:41:58 +0800

    managed to get token

--- ZoomService.js
+++ ZoomService.js
@@ -1,15 +1,20 @@
 import axios from "axios";
+import "dotenv/config";
 
 async function getAccessToken() {
   const data = new URLSearchParams();
 
   data.append("grant_type", "account_credentials");
-  data.append("account_id", "account_id");
+  data.append("account_id", process.env.ZOOM_ACCOUNT_ID);
+
+  // encode credentials to base64
+  const credentials = `${process.env.ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`;
+  const encoded = btoa(credentials);
 
   const config = {
     headers: {
       Host: "zoom.us",
-      Authorization: `Basic 123`,
+      Authorization: `Basic ${encoded}`,
     },
   };
   const res = await axios.post("https://zoom.us/oauth/token", data, config);
